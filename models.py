c5cfe1ee2dc1007b8c6a6612699bb7633adbfcf8
PythoniCode <anthony.j.riley@gmail.com>
2022-03-08 16:50:20 -0800

    2nd Run - Still Plateau

--- models.py
+++ models.py
@@ -5,17 +5,19 @@ import wandb
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optimizers
 from sklearn import metrics
-from transformers import AutoModel, AdamW, get_cosine_schedule_with_warmup
+from transformers import AutoModel, get_cosine_schedule_with_warmup
 
 class BigLongBirdFormer(tez.Model):
     def __init__(self, longformer_path, bigbird_path, num_labels, n_steps):
         super().__init__()
         self.longformer = AutoModel.from_pretrained(longformer_path)
         self.bigbird = AutoModel.from_pretrained(bigbird_path)
+        self.sequence_drop = nn.Dropout(p=0.2)
         self.output = nn.Linear(self.longformer.config.hidden_size +  self.bigbird.config.hidden_size, num_labels)
-        self.n_steps = n_steps
         self.num_labels = num_labels
+        self.n_steps = n_steps
 
     def monitor_metrics(self, logits, labels):
         if logits is None:
@@ -28,34 +30,17 @@ class BigLongBirdFormer(tez.Model):
         return { "accuracy": accuracy }
     
     def fetch_scheduler(self):
-        return get_cosine_schedule_with_warmup(
-            self.optimizer, num_warmup_steps=0, num_training_steps=self.n_steps
-        )
+        return get_cosine_schedule_with_warmup(self.optimizer, num_warmup_steps=0, num_training_steps=self.n_steps)
     
     def fetch_optimizer(self):
-        params = list(self.named_parameters())
-        no_decay = ["bias", "LayerNorm.bias"]
-        optimize = [
-            {
-                "params": [
-                    p for n, p in params if not any(nd in n for nd in no_decay)
-                ],
-                "weight_decay": 0.001,
-            },
-            {
-                "params": [
-                    p for n, p in params if any(nd in n for nd in no_decay)
-                ],
-                "weight_decay": 0.0,
-            },
-        ]
-        return AdamW(optimize) 
+        params = [param[1] for param in self.named_parameters()]
+        return optimizers.AdamW(params)
 
     def forward(self, input_ids, attention_mask, labels=None):
         longformer_out = self.longformer(input_ids, attention_mask)
         bigbird_out = self.bigbird(input_ids, attention_mask)
         sequence_output = torch.cat((longformer_out.last_hidden_state, bigbird_out.last_hidden_state), dim=2)
-        logits = self.output(sequence_output)
+        logits = self.output(self.sequence_drop(sequence_output))
         logits = torch.permute(logits, (0, 2, 1))
         loss = nn.CrossEntropyLoss()(logits, labels)
         wandb.log({"loss": loss})
